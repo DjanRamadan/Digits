fd1a43b11d51eaabe4d1ded8dbb400a720684a75
Djan <124775935+DjanRamadan@users.noreply.github.com>
2023-03-01 11:37:26 +0200

    Add files via upload

---
+++ Zad2.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0, num;
+
+while (number > 0)
+{
+    num = number % 10;
+    sum = sum + num;
+    number = number / 10;
+}
+
+Console.WriteLine($"The sum of the digits is: {sum}");
